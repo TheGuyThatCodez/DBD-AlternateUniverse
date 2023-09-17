80db33268577c4fbd88718db43a5018659184818
TheGuyThatCodez <134526423+TheGuyThatCodez@users.noreply.github.com>
2023-09-17 09:03:13 -0700

    Update entities.lua

--- entities.lua
+++ entities.lua
@@ -2,7 +2,7 @@ local dbdmods = assert(loadstring(game:HttpGet('https://raw.githubusercontent.co
 
 
 local entity = DBD_Templates.Rush
-entity.Model = game:HttpGet('https://raw.githubusercontent.com/TheGuyThatCodez/DBD-AlternateUniverse/blob/main/Ripper.rbxm')
+entity.Model = game:HttpGet('https://raw.githubusercontent.com/TheGuyThatCodez/DBD-AlternateUniverse/master/Ripper.rbxm')
 entity.Speed = 2
 entity.Kills = false
 entity.PlaySounds = {"Static","Near"}
